8c4815fcde3393c7a704e4c6a454f8b015342cc7
Thanusri Veera <thanusriveera1@gmail.com>
2026-06-25 22:55:32 +0530

    Add CodSoft Task 2 Calculator with all 4 operations

---
+++ Calculator.py
@@ -0,0 +1,46 @@
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    if y == 0:
+        return "Error: Cannot divide by zero!"
+    return x / y
+
+print("==CALCULATOR==")
+print("Select operation:")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("\n Enter choice 1/2/3/4:")
+      
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter numbers only.")
+            continue
+        if choice == '1':
+            print(f"{num1} + {num2} = {add(num1,num2)}")
+        elif choice == '2':
+            print(f"{num1} - {num2} = {subtract(num1,num2)}") 
+        elif choice == '3':
+            print(f"{num1} * {num2} = {multiply(num1,num2)}") 
+        elif choice == '4':
+            print(f"{num1} / {num2} = {divide(num1,num2)}") 
+            
+        next_calc = input("\n Do another calculation? yes/no: ")
+        if next_calc.lower() != 'yes':
+            print("Thank you for using CodSoft Calculator!")
+            break
+    else:
+        print("Invalid choice. Please select 1, 2, 3, or 4")
